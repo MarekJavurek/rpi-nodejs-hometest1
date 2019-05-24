ad77d386c29080992f22da4defc40d76bfe553e3
Marek Javůrek <m.javurek@ndtcloud.net>
2019-05-25 00:36:44 +0200

    port 80

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 80;
 
 process.title = "myApp";
 
-app.get('/', (req, res) => res.send('Hello World! 5'))
+app.get("/", (req, res) => res.send("Hello World! 5"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
