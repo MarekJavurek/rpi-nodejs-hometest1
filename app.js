fa16fbbfb74733d7109eebb694a4f17f118c7cbf
Marek Javůrek <m.javurek@ndtcloud.net>
2019-05-25 19:55:27 +0200

    DHT22

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 const port = "8080";
+var sensor = require("node-dht-sensor");
 
-app.get("/", (req, res) => res.send("Hello World! PM2 !!!"));
+app.get("/", (req, res) => {
+  sensor.read(22, 2, function(err, temperature, humidity) {
+    if (!err) {
+      res.send(
+        "temp: " +
+          temperature.toFixed(1) +
+          "°C, " +
+          "humidity: " +
+          humidity.toFixed(1) +
+          "%"
+      );
+    } else {
+      res.send("Cant read DHT22");
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
