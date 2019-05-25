2bbdfeb6675aa81dcb170cbda5dbbc5effe11733
Marek Javůrek <m.javurek@ndtcloud.net>
2019-05-25 02:28:58 +0200

    pm2 stop rpi

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#This is a simple bash script that will poll github for changes to your repo,
-#if found pull them down, and then rebuild and restart 
-#We run the script with nohup so it executes as a background process: $nohup ./runscript
 
 echo RUNSCRIPT started
 while true
@@ -16,16 +13,14 @@ REMOTE=$(git rev-parse @{u});
 
 #if our local revision id doesn't match the remote, we will need to pull the changes
 if [ $LOCAL != $REMOTE ]; then
-    pm2 stop all;
+    pm2 stop rpi;
     git fetch --all;
 	git reset --hard origin/master;
     npm install;
-    pm2 start app.js;
+    pm2 start app.js --name "rpi";
 fi
 echo RUNSCRIPT sleep 5 sec
 sleep 5
 done
 echo RUNSCRIPT ended
 
-# @reboot /usr/bin/sudo -u pi -H /usr/local/bin/forever start -c /bin/bash ~/rpi-nodejs-hometest1/runscript
-# sudo crontab -e
