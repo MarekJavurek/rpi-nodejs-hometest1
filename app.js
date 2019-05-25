e105568603da113caa0231bc711b6506532c9274
Marek Javůrek <m.javurek@ndtcloud.net>
2019-05-25 20:38:32 +0200

    fix main route

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
 const port = "8080";
-
-const sensor = require("node-dht-sensor").promises;
+var sensor = require("node-dht-sensor").promises;
 sensor.setMaxTries(10);
 
-var winston = require("winston");
+/* var winston = require("winston");
 
 var logger = new winston.Logger({
   level: "error",
   transports: [new winston.transports.File({ filename: "error.log" })]
-});
+}); */
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send(`RPI ZERO W app listening on port ${port}!`);
 });
 
@@ -26,8 +25,8 @@ app.get("/get/:gpioPin", async (req, res) => {
     const { gpioPin } = req.params;
     const { temperature, humidity } = await sensor.read(22, gpioPin);
 
-    logger.log("error", "temperature:  %s", temperature);
-    logger.log("error", "humidity:  %s", humidity);
+    //logger.log("error", "temperature:  %s", temperature);
+    //logger.log("error", "humidity:  %s", humidity);
 
     res.send(
       "temp: " +
@@ -39,7 +38,7 @@ app.get("/get/:gpioPin", async (req, res) => {
     );
   } catch (err) {
     res.send("Failed to read sensor data: " + err);
-    logger.log("error", "Failed to read sensor data:  %s", err);
+    //logger.log("error", "Failed to read sensor data:  %s", err);
   }
 });
 
