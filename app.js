04b7bccb8f99411029a792f8491a77aab368004a
Marek Javůrek <m.javurek@ndtcloud.net>
2019-05-25 20:28:03 +0200

    added winston

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const port = "8080";
-var sensor = require("node-dht-sensor").promises;
-sensor.setMaxTries(3);
+
+const sensor = require("node-dht-sensor").promises;
+sensor.setMaxTries(10);
+
+var winston = require("winston");
+
+var logger = new winston.Logger({
+  level: "error",
+  transports: [new winston.transports.File({ filename: "error.log" })]
+});
 
 app.get("/sync", (req, res) => {
   var readout = sensor.readSync(22, 2);
@@ -12,6 +20,10 @@ app.get("/sync", (req, res) => {
 app.get("/", async (req, res) => {
   try {
     const { temperature, humidity } = await sensor.read(22, 2);
+
+    logger.log("error", "temperature:  %s", temperature);
+    logger.log("error", "humidity:  %s", humidity);
+
     res.send(
       "temp: " +
         temperature.toFixed(1) +
@@ -22,7 +34,10 @@ app.get("/", async (req, res) => {
     );
   } catch (err) {
     res.send("Failed to read sensor data: " + err);
+    logger.log("error", "Failed to read sensor data:  %s", err);
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`RPI ZERO W app listening on port ${port}!`)
+);
