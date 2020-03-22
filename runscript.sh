c345331b47f32c7f95dd401d643d75d325ee38dd
Marek Javůrek <peanee@gmail.com>
2020-03-22 01:19:35 +0100

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -16,14 +16,18 @@ REMOTE=$(git rev-parse @{u});
 
 #if our local revision id doesn't match the remote, we will need to pull the changes
 if [ $LOCAL != $REMOTE ]; then
-    pm2 stop rpi;
-    git fetch --all;
+	pm2 stop rpi;
+	git fetch --all;
 	git reset --hard origin/master;
-    npm install;
-    pm2 start appWeb.js --name "rpi";
+	npm install;
+	pm2 start appWeb.js --name "rpi";
+else
+	if [ "$(pm2 id rpi) "= "[]" ]; then
+		pm2 start appWeb.js --name "rpi";
+	fi
 fi
+
 echo RUNSCRIPT sleep 5 sec
 sleep 5
 done
 echo RUNSCRIPT ended
-
